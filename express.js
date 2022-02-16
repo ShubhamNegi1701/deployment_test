a879871a5fcd8a67ea2ac35be657d34fc5819624
Shubham <shubham@salyxmedical.com>
2022-02-15 16:45:59 -0800

    first commit

--- express.js
+++ express.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-//import path
+//import path or 5000
 const path = process.env.PORT || 5000;
 
+//if you are deploying to heroku, serve index.html file from build folder not public folder
 if(process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
@@ -11,7 +12,11 @@ if(process.env.NODE_ENV === "production") {
         })
 }
 
-app.listen(port, (err) => {
+//starting a server and serving the public folder
+app.use(express.static(__dirname + '/public'));
+
+//if there is a server already running on the specified port, show an error message
+app.listen(process.env.PORT || 5000, (err) => {
     if (err) return console.log(err);
-    console.log('Server running on port:', port);
+    console.log('Server running on port:', process.env.PORT || 5000);
 })
